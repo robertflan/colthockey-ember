3f41204b044644314b9dab51e450c4a177ba8642
onestardev <faonestar514@gmail.com>
2017-10-23 19:28:08 +0200

    exam for route

--- app/router.js
+++ app/router.js
@@ -1,12 +1,17 @@
-import EmberRouter from '@ember/routing/router';
+import Ember from 'ember';                    
+//Access to Ember.js library as variable Ember
 import config from './config/environment';
+//It provides access to app's configuration data as variable config 
 
-const Router = EmberRouter.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
+//The const declares read only variable
+const Router = Ember.Router.extend ({
+   location: config.locationType,
+   rootURL: config.rootURL
 });
 
+//Defines URL mappings that takes parameter as an object to create the routes
 Router.map(function() {
+   this.route('index');
 });
 
 export default Router;
