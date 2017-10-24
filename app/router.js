d91395ecab72d27e668e77b7e3e8b7dabb0df494
onestardev <faonestar514@gmail.com>
2017-10-24 21:21:25 +0200

    nestedroute

--- app/router.js
+++ app/router.js
@@ -1,7 +1,7 @@
-import Ember from 'ember';                    
+import Ember from 'ember';                   
 //Access to Ember.js library as variable Ember
 import config from './config/environment';
-//It provides access to app's configuration data as variable config 
+//It provides access to app's configuration data as variable config
 
 //The const declares read only variable
 const Router = Ember.Router.extend ({
@@ -11,7 +11,10 @@ const Router = Ember.Router.extend ({
 
 //Defines URL mappings that takes parameter as an object to create the routes
 Router.map(function() {
-   this.route('index');
+   this.route('nestedroute', function() {
+      this.route('fruits');
+   });
 });
 
+//It specifies Router variable available to other parts of the app
 export default Router;
