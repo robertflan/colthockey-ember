222b3934d5dd45ca5831ad2f275cbd23c6de0a09
onestardev <faonestar514@gmail.com>
2017-10-26 23:21:28 +0200

    chaining computed property

--- app/app.js
+++ app/app.js
@@ -1,27 +1,26 @@
 import Ember from 'ember';
 
 export default function() {
-   //reopen() method for instances
-   var Person = Ember.Object.extend ({
-      firstName: null,
-      lastName:  null,
+   var Car = Ember.Object.extend ({
+      
+      //The values for below variables will be supplied by 'create' method
+      CarName: null,
+      CarModel: null,
+      carDetails: Ember.computed('CarName', 'CarModel', function() {
+
+         //returns values to the computed property function 'carDetails'
+         return ' Car Name: ' + this.get('CarName') + '<br>' + 
+            ' Car Model: ' + this.get('CarModel');
+      })
    });
 
-   //adding new variable to the Person class
-   Person.reopen ({
-      middleName: 'Smith',
+   var mycar = Car.create ({
+      //initializing the values of Car variables
+      CarName: "Alto",
+      CarModel: "800",
    });
-
-   document.write('Middle Name: '+Person.create().get('middleName'));
-   document.write("<br>");
-
-   //reopenClass() method for classes
-   Person.reopenClass ({
-      //creating new function for class Person
-      openClass: function() {
-         return Person.create({isMan: true});
-      }
-   });
-
-   document.write('isMan: '+Person.openClass().get('isMan'));
+   
+   //Displaying the information of the car
+   document.write("<h2>Details of the car: <br></h2>");
+   document.write(mycar.get('carDetails'));
 }
