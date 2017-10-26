ae0b60da4c4a9e9d99a7d53c2def69edacc1986d
onestardev <faonestar514@gmail.com>
2017-10-26 23:18:17 +0200

    reopening class

--- app/app.js
+++ app/app.js
@@ -1,16 +1,27 @@
-import classinstance from './classinstance';
-import Ember from 'ember';   //import ember module
+import Ember from 'ember';
+
 export default function() {
-   
-   //new ember object
-   const Demo = Ember.Object.extend ({
-      init() {
-         alert('The default property of stateOn is : ' + this.get('stateOn'));
-      },
-      stateOn: false
+   //reopen() method for instances
+   var Person = Ember.Object.extend ({
+      firstName: null,
+      lastName:  null,
    });
 
-   const state = Demo.create();   //new instance from object with create() method
-   state.set('stateOn', true);
-   console.log(state.get('stateOn'));
+   //adding new variable to the Person class
+   Person.reopen ({
+      middleName: 'Smith',
+   });
+
+   document.write('Middle Name: '+Person.create().get('middleName'));
+   document.write("<br>");
+
+   //reopenClass() method for classes
+   Person.reopenClass ({
+      //creating new function for class Person
+      openClass: function() {
+         return Person.create({isMan: true});
+      }
+   });
+
+   document.write('isMan: '+Person.openClass().get('isMan'));
 }
